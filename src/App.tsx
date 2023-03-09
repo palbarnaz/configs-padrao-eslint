2d531477a129dba07dbdcb92e783ee7bab189817
Paola Albarnaz <110476307+palbarnaz@users.noreply.github.com>
2023-03-09 14:30:56 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppRoutes from './Routes/AppRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppRoutes/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
