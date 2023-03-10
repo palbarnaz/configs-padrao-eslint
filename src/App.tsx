5d53743c9d238e9fa9d760e89f8e3b02549cb5a3
Paola Albarnaz <paola.silva.nh1@gmail.com>
2023-03-10 09:06:24 -0300

    alterando config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
+
 import AppRoutes from './Routes/AppRoutes';
- 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AppRoutes/>
-      </header>
-    </div>
-  ); 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <AppRoutes />
+            </header>
+        </div>
+    );
 }
 
 export default App;
